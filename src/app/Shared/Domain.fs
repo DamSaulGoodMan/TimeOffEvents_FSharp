e6b0aaa5432aed752e1d49a308dceb672c2cd42e
DamSaulGoodMan <capron.damien@laposte.net>
2019-09-29 22:12:52 +0200

    Commit base file of project Add new types and start modif on Model.fs

---
+++ src/app/Shared/Domain.fs
@@ -0,0 +1,64 @@
+﻿namespace TimeOff
+
+open System
+
+// First, we define our domain
+type UserId = string
+
+type User =
+    | Employee of UserId
+    | Manager
+
+type HalfDay = | AM | PM
+
+[<CLIMutable>]
+type Boundary = {
+    Date: DateTime
+    HalfDay: HalfDay
+}
+
+
+// Employed
+[<CLIMutable>]
+type TimeOffRequest = {
+    UserId: UserId
+    RequestId: Guid
+    Start: Boundary
+    End: Boundary
+}
+
+[<CLIMutable>]
+type TimeOffCancel = {
+    UserId: UserId
+    RequestId: Guid
+}
+
+[<CLIMutable>]
+type TimeOffRequestCancel = {
+    UserId: UserId
+    RequestId: Guid
+}
+
+
+// Manager
+[<CLIMutable>]
+type TimeOffValidate = {
+    UserId: UserId
+    RequestId: Guid
+    Manager: User
+}
+
+[<CLIMutable>]
+type TimeOffRefuse = {
+    UserId: UserId
+    RequestId: Guid
+    Manager: User
+}
+
+[<CLIMutable>]
+type TimeOffRefuseCancel = {
+    UserId: UserId
+    RequestId: Guid
+    Manager: User
+}
+
